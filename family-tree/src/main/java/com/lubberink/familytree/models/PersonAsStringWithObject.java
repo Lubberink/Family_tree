ea9fec10c28bfaf1f5a516ef0c91bfb69304ca40
GLubberink <gerco100@gmail.com>
2022-08-26 11:55:41 +0200

    new objecttype introduced

---
+++ family-tree/src/main/java/com/lubberink/familytree/models/PersonAsStringWithObject.java
@@ -0,0 +1,6 @@
+package com.lubberink.familytree.models;
+
+public class PersonAsStringWithObject {
+    private String personAsString;
+    private Person person;
+}
--- family-tree/src/main/java/com/lubberink/familytree/models/interfaces/ModelValue.java
+++
@@ -1,4 +0,0 @@
-package com.lubberink.familytree.models.interfaces;
-
-public interface ModelValue {
-}
